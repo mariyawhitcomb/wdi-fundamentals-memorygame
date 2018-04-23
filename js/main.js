d5687b5ff2eb3274401882aa095117b62976820f
Mariya Whitcomb <mariyawhitcomb@Mariyas-MacBook-Pro.local>
2018-04-23 14:17:43 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+
+var checkForMatch = function () {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -14,4 +10,13 @@ if (cardsInPlay.length === 2) {
 else { alert("Sorry, try again.");
 	}
 }
+}
 
+var flipCard = function (cardId) { 
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
